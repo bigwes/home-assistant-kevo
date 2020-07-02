48f700bee95e5870df4c355f79bc6676bd757bd9
davidlang42 <37281992+davidlang42@users.noreply.github.com>
2020-07-02 20:37:53 +1000

    Add optimistic state changes

--- custom_components/Kevo/lock.py
+++ custom_components/Kevo/lock.py
@@ -13,6 +13,7 @@ _LOGGER = logging.getLogger(__name__)
 CONF_LOCK_ID = "lock_id"
 CONF_MAX_RETRIES = "max_retries"
 CONF_RETRY_DELAY = "retry_delay"
+CONF_OPTIMISTIC = "optimistic"
 
 # Validation of the user's configuration
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
@@ -20,7 +21,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_PASSWORD): cv.string,
     vol.Required(CONF_LOCK_ID): cv.string,
     vol.Optional(CONF_MAX_RETRIES, default=3): cv.positive_int,
-    vol.Optional(CONF_RETRY_DELAY, default=2): cv.positive_int
+    vol.Optional(CONF_RETRY_DELAY, default=2): cv.positive_int,
+    vol.Optional(CONF_OPTIMISTIC, default=True): cv.boolean
 })
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
@@ -33,6 +35,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     lock_id = config.get(CONF_LOCK_ID)
     max_retries = config.get(CONF_MAX_RETRIES)
     retry_delay = config.get(CONF_RETRY_DELAY)
+    optimistic = config.get(CONF_OPTIMISTIC)
 
     # Setup connection with devices/cloud (broken as of 9 Sep 2019 due to CAPTCHA changes)
     # kevos = Kevo.GetLocks(email, password)
@@ -50,16 +53,17 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         else:
             break
     
-    add_devices([KevoDevice(kevo)])
+    add_devices([KevoDevice(kevo, optimistic)])
 
 class KevoDevice(LockDevice):
     """Representation of a Kevo Lock."""
 
-    def __init__(self, kevo):
+    def __init__(self, kevo, optimistic):
         """Initialize an Kevo Lock."""
         self._kevo = kevo
         self._name = kevo.name
         self._state = None
+        self._optimistic = optimistic
 
     @property
     def name(self):
@@ -75,19 +79,25 @@ class KevoDevice(LockDevice):
         """Instruct the lock to lock."""
         with KevoLockSession(self._kevo):
             self._kevo.Lock()
-            self._state = self._kevo.GetBoltState().lower()
+            if self._optimistic:
+                self._state = "locked"
+            else:
+                self._state = self._kevo.GetBoltState().lower()
 
     def unlock(self, **kwargs):
         """Instruct the lock to unlock."""
         with KevoLockSession(self._kevo):
             self._kevo.Unlock()
-            self._state = self._kevo.GetBoltState().lower()
+            if self._optimistic:
+                self._state = "unlocked"
+            else:
+                self._state = self._kevo.GetBoltState().lower()
 
     def update(self):
         """Fetch new state data for this lock.
 
         This is the only method that should fetch new data for Home Assistant.
         """
-        if self._state == "unlocked":
+        if self._state != "locked":
             with KevoLockSession(self._kevo):
                 self._state = self._kevo.GetBoltState().lower()
